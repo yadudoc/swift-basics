9d24a80cebe2d653d31a71ead82abb572ca6a094
Yadu Nand B <yadu@Miranda.(none)>
2013-03-02 00:47:05 +0530

    x_recursion does not stress enough yet.

--- stress/x_recursion.swift
+++ stress/x_recursion.swift
@@ -1,6 +1,9 @@
-
-(int out) sum (int in){
-     out = in ;
+(int out) sum (int n){
+     if ( n == 0 ){
+     	out = 0;
+     }else{
+          out = n + sum( n-1 );
+     }
 }
 
 int result = sum(100);
