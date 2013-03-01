81a7584ee57df81ad0c2417c27326d3dc14e9b4f
Yadu Nand B <yadu@Miranda.(none)>
2013-03-01 23:37:32 +0530

    Added x_sleep for measuring performance with a delay app.
    Tests run with x_iterate show OutOfMemory crashes at 1M calls.

--- stress/x_iterate.swift
+++ stress/x_iterate.swift
@@ -1,6 +1,12 @@
 /*
 	Iterate loops with variable number of loops
 	to be called with cmdline arg -loops=<Number>
+
+	todo : Fails to compile on 0.94
+	       Compiles on 0.93 (Regression noted)
+	stats: 100k -> 17s (core i5)
+	       1M   -> java.langOutOfMemoryError thrown
+
 */
 
 int limit   = @toint(@arg("loops"));
@@ -8,7 +14,8 @@ int result[];
 
 iterate current {
 	result[current] = current;
+	//	tracef("result[current] = %i \n", result[current]);
 } until ( current > limit );
 
-tracef("result[4] = %i \n", result[4]);
-tracef("result[5] = %i \n", result[5]);
+tracef("Result[0]    = %i \n", result[0]);
+tracef("Result[%i]   = %i \n", limit, result[limit]);
--- stress/x_sleep.swift
+++ stress/x_sleep.swift
@@ -1,15 +1,17 @@
 /*
 	Foreach loops with variable number of loops
 	to be called with cmdline arg -loops=<Number>
+	todo: measure actual times with expected times.
 */
 
 int limit   = @toint(@arg("loops")); 
 int range[] = [0:limit:1];
 int result[];
 
-foreach num, index in range {
-	result[index] = index;
+app sleep (int seconds){
+    sleep seconds;
 }
 
-tracef("Result[0]   = %i \n", result[0]);
-tracef("Result[%i]  = %i \n", limit, result[limit]);
+foreach num in range {
+	sleep(num%%5);
+}
